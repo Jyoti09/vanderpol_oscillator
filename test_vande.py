c9ce7a14f592dce9dbf67336af364ed8d7d4631c
Jyoti <Jyoti.dhakal09@gmail.com>
2016-10-22 19:28:12 +0530

    The test file

---
+++ test_vande.py
@@ -0,0 +1,16 @@
+import unitest
+import vande_plot as plt
+
+class Test van(unittest.Testcase):
+    
+    """ Test class for testing vander plot
+    """
+    
+    def solve_differential_equation(self):
+        for i in range(0,2):
+        self.assertAlmostEqual(soln_x[i],0)
+    
+    
+if __name__=='__main__'
+
+unitest.main()
